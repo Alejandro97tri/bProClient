ad6bacb9d5f7c8a8ae61d970ab31da7e74173bf2
Alejandro97tri <alejandrof97g8@gmail.com>
2023-05-08 17:20:38 +0200

    Home atleta 2

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   userLoged: any;
 
+  
   constructor() {
     let session = sessionStorage.getItem('auth');
     if (session !== null) {
       this.userLoged = JSON.parse(session);
+      console.log(this.userLoged.id);
+      
     }
+  }
+
+  ngOnInit () {
+    this.getAtletas();
+  }
+
+  async getAtletas(){
+
+      const response = await fetch('https://btop.es/server/home.php', { method: 'POST', body: JSON.stringify({'id': this.userLoged.id})});
+      const data = await response.json();
+      console.log(data);
   }
 }
 

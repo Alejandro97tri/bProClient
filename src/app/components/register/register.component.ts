4186bf71193fc31dcdc7b02615cb55559214f5c5
Alejandro97tri <alejandrof97g8@gmail.com>
2023-05-18 17:15:30 +0200

    Registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  deporte: string = '';
+  username: any;
+  password: any;
+  nombre: any;
+  apellidos: any;
+  fecha_nacimiento: any;
+  email: any;
+  genero: string = '';
+  rol: string = '';
+  peso: any;
+  altura: any;
+
+
+  listaDeportes: any;
+
+  ngOnInit(): void {
+    this.getListaDeportes();
+  }
+
+  async getListaDeportes() {
+    const response = await fetch('https://btop.es/server/listaDeportes.php', { method: 'POST' });
+    this.listaDeportes = await response.json();
+    console.log(this.listaDeportes);
+  }
+
+  enviar(){
+    
+  }
 }
